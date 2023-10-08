cb12807e0f115af38780d39b301b556cff371fec
Gergö Kadar <kadargergo20@gmail.com>
2023-10-08 16:20:44 +0200

    Tilføjet de nødvendige metoder i Gameboard.java

--- TicTacToe/TicTacToe/src/tictactoe/bll/GameBoard.java
+++ TicTacToe/TicTacToe/src/tictactoe/bll/GameBoard.java
@@ -11,7 +11,13 @@ package tictactoe.bll;
  */
 public class GameBoard implements IGameModel
 {
+    private int currentPlayer = 0;
+    private int [][] board;
 
+    public GameBoard ()
+    {
+        board = new int[3][3];
+    }
     /**
      * Returns 0 for player 0, 1 for player 1.
      *
@@ -19,8 +25,7 @@ public class GameBoard implements IGameModel
      */
     public int getNextPlayer()
     {
-        //TODO Implement this method
-        return 0;
+        return currentPlayer;
     }
 
     /**
@@ -36,12 +41,62 @@ public class GameBoard implements IGameModel
     public boolean play(int col, int row)
     {
         //TODO Implement this method
-        return true;
+        if(board[row][col] == 0)
+        {
+            board[row][col] = currentPlayer + 1;
+            currentPlayer = 1 - currentPlayer;
+            return true;
+
+            //haha
+            //test 1
+        }
+        return false;
     }
 
     public boolean isGameOver()
     {
-        //TODO Implement this method
+        for(int row = 0; row < 3; row++)
+        {
+            if(board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] !=0)
+            {
+                return true;
+            }
+        }
+
+        for (int col = 0; col < 3; col++)
+        {
+            if(board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] !=0)
+            {
+                return true;
+            }
+        }
+
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0)
+        {
+            return true;
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0)
+        {
+            return true;
+        }
+
+        boolean isDraw = true;
+        for(int row = 0; row < 3; row++)
+        {
+            for(int col = 0; col < 3; col++)
+            {
+                if(board[row][col] == 0)
+                {
+                    isDraw = false;
+                    break;
+                }
+            }
+        }
+        if(isDraw == true)
+        {
+            return true;
+        }
         return false;
     }
 
@@ -52,7 +107,32 @@ public class GameBoard implements IGameModel
      */
     public int getWinner()
     {
-        //TODO Implement this method
+        for(int row = 0; row < 3; row++)
+        {
+            if(board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] !=0)
+            {
+                return board[row][0] - 1;
+            }
+        }
+
+        for (int col = 0; col < 3; col++)
+        {
+            if(board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] !=0)
+            {
+                return board[0][col] -1;
+            }
+        }
+
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0)
+        {
+            return board[0][0] -1;
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0)
+        {
+            return board[0][2] - 1;
+        }
+
         return -1;
     }
 
@@ -61,7 +141,17 @@ public class GameBoard implements IGameModel
      */
     public void newGame()
     {
-        //TODO Implement this method
+
+        for(int row = 0; row < 3; row++)
+        {
+            for(int col = 0; col < 3; col++)
+            {
+                if (board[row][col] != 0)
+                {
+                    board[row][col] = 0;
+                }
+            }
+        }
     }
 
 }
